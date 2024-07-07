b141541988a5aa97e36894ce71ff433f3548623e
umutakinc <umutakinci5@gmail.com>
2024-07-07 17:02:17 +0300

    repeatString function add and test successful

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatNum) {
+    let result = "";
+    if (repeatNum < 0) {
+        result = "ERROR";
+    }
+    for (let i = 0; i < repeatNum; i++) {
+        result += str;
+    }
+    return result
 };
 
 // Do not edit below this line
