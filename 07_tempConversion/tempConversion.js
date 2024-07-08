0c21cc9b63c26b5cd6bfa9b66a1e4af50b0c9998
umutakinc <umutakinci5@gmail.com>
2024-07-08 23:41:20 +0300

    tempConversion functions add and test succesfull

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempValue) {
+  let toCelsius = (tempValue - 32) * (5 / 9);
+
+  if (toCelsius !== 0) {
+    toCelsius = +toCelsius.toFixed(1);
+  }
+
+  return toCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempValue) {
+  let toFahrenheit = tempValue * (9 / 5) + 32;
+
+  if (toFahrenheit !== 0) {
+    toFahrenheit = +toFahrenheit.toFixed(1);
+  }
+
+  return toFahrenheit
 };
 
 // Do not edit below this line
