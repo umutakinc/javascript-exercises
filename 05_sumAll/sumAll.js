3b39d1a91d0360293349df774732a88919795bf7
umutakinc <umutakinci5@gmail.com>
2024-07-08 23:17:26 +0300

    sumAll function add and test successfull

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    // Başlangıç ve bitiş değerlerini alarak o aralıktaki sayıları toplayan fonksiyon. Negatif değerler ve sayı olmayan değerler kabul edilmez.
+
+    let result = 0;
+
+    if (
+        (startNum < 0 || endNum < 0)
+        || 
+        (typeof startNum !== 'number' || typeof endNum !== 'number')
+    ) {
+        return "ERROR"
+    }
+
+    if (startNum > endNum) {
+        for (let i = endNum; i <= startNum; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = startNum; i <= endNum; i++) {
+            result += i;
+        }
+    }
+
+
+    return result
 };
 
 // Do not edit below this line
